44d214c82475023975d54e77b347759ceda0b739
Carlos Dugarte <soycarlosdugarte@gmail.com>
2022-11-14 19:20:20 -0300

    Se optimiza el codigo

--- src/components/examples/MultipleCustomHooks.jsx
+++ src/components/examples/MultipleCustomHooks.jsx
@@ -1,5 +1,7 @@
-import { useCounter } from "../../hooks/useCounter"
-import { useFetch } from "../../hooks/useFetch"
+import { useCounter, useFetch } from "../../hooks"
+
+//components
+import { LoadingQuote, Quote } from "./"
 
 export const MultipleCustomHooks = () => {
 
@@ -14,17 +16,11 @@ export const MultipleCustomHooks = () => {
       <hr />
       {
         isLoading
-          ? (
-            <div className="alert alert-info text-center">Loading...</div>
-          )
-          : (
-      <blockquote className="blockquote text-end">
-        <p className="mb-4">{ quote }</p>
-        <footer className="blockquote-footer">
-          { author }
-        </footer>
-      </blockquote>
-          )
+          ? <LoadingQuote />
+          : <Quote
+              author={ author }
+              quote={ quote }
+            />
       }
 
       <button
